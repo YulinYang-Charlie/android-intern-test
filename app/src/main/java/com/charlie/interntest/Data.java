1f274411b8df500a3b3b4fe12257bff00825db0f
YulinYang-Charlie <42225955+YulinYang-Charlie@users.noreply.github.com>
2019-08-19 00:07:27 +1000

    import data

--- app/src/main/java/com/charlie/interntest/Data.java
+++ app/src/main/java/com/charlie/interntest/Data.java
@@ -2,6 +2,10 @@ package com.charlie.interntest;
 
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,12 +18,26 @@ public class Data extends AsyncTask<Void,Void,Void> {
 
     String data = "";
 
+    int[] id = new int[5000] ;
+    String[] url = new String[5000];
+    String[] name = new String[5000];
+    int[] season = new int[5000];
+    int[] number = new int[5000];
+    String[] airdate = new String[5000];
+    String[] airtime = new String[5000];
+    String[] airstamp = new String[5000];
+    int[] runtime = new int[5000];
+    String[] medium = new String[5000];
+    String[] original = new String[5000];
+    String[] summary = new String[5000];
+    String[] links = new String[5000];
+
     @Override
     protected Void doInBackground(Void... voids) {
 
         try {
-            URL url = new URL("http://api.tvmaze.com/shows/1/episodes");
-            HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            URL url1 = new URL("http://api.tvmaze.com/shows/1/episodes");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) url1.openConnection();
             InputStream inputStream = httpURLConnection.getInputStream();
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
             String line = "";
@@ -29,11 +47,34 @@ public class Data extends AsyncTask<Void,Void,Void> {
                 data = data + line;
             }
 
+            JSONArray JA= new JSONArray(data);
+            JSONObject images;
+            for (int i = 0; i < JA.length(); i++) {
+                JSONObject JO = JA.getJSONObject(i);
+                id[i] = JO.getInt("id");
+                url[i] = JO.getString("url");
+                name[i] = JO.getString("name");
+                season[i] = JO.getInt("season");
+                number[i] = JO.getInt("number");
+                airdate[i] = JO.getString("airdate");
+                airtime[i] = JO.getString("airtime");
+                airstamp[i] = JO.getString("airstamp");
+                runtime[i] = JO.getInt("runtime");
+                images = JO.getJSONObject("image");
+                medium[i] = images.getString("medium");
+                original[i] = images.getString("original");
+                summary[i] = JO.getString("summary");
+                links[i] = JO.getJSONObject("_links").getJSONObject("self").getString("href");
+
+            }
+
 
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
             e.printStackTrace();
         }
 
